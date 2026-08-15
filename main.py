b4411841ee6d27c09547a2e842d9fc65e488680c
cvl196 <sfilipp2008@yandex.ru>
2026-08-15 13:27:18 +0500

    update
    del func
    try except
    back button

--- main.py
+++ main.py
@@ -22,6 +22,18 @@ def create_keyboard_main():
     keyboard.add(telebot.types.InlineKeyboardButton(text="Настроить цель", callback_data="target")) 
     return keyboard
 
+def create_keyboard_back():
+    keyboard = telebot.types.InlineKeyboardMarkup()
+    keyboard.add(telebot.types.InlineKeyboardButton(text = 'В главное меню',callback_data='back'))
+    return keyboard
+
+def create_keyboard_del(items): 
+    keyboard = telebot.types.InlineKeyboardMarkup()
+    for item in items: 
+        keyboard.add(telebot.types.InlineKeyboardButton(text = item[0], callback_data=f'del%{item[1]}'))
+    return keyboard
+
+
 
 
 @bot.message_handler(commands=['help', 'start'])
@@ -38,28 +50,68 @@ def echo_message(message):
 @bot.callback_query_handler(func=lambda call: True)
 def test_callback(call): 
     if call.data == "add":
-        bot.send_message(call.message.chat.id, "Укажите прием пищи и кбжу в формате: Обед: 500 50 20 45")
-        bot.register_next_step_handler(call.message, add_meal)
-    if call.data == "mypr":
-        date = datetime.datetime.now().strftime("_%d_%m_%y")
+        try:
+            bot.send_message(call.message.chat.id, "Укажите прием пищи и кбжу в формате: Обед: 500 50 20 45")
+            bot.register_next_step_handler(call.message, add_meal)
+        except: 
+            bot.send_message(call.message.chat.id,'Неизвестная ошиибка')
 
-        with sqlite3.connect('database.db') as conn:
-            cu = conn.cursor() 
-            cu.execute(f"SELECT meal,info FROM MEALS{date} WHERE chat_id = ?", (call.message.chat.id,))
-            meals = cu.fetchall()
-            text = "Ваши приемы пищи:\n"
-            for meal in meals: 
-                text += f"{meal[0]}: {meal[1]}\n"
-            bot.send_message(call.message.chat.id, text)
+    elif call.data=='back':
+        try:
+            bot.send_message(call.message.chat.id, "Добрый день, выберите действие:", reply_markup=create_keyboard_main())
+        except:
+            bot.send_message(call.message.chat.id,'Неизвестная ошибка')
             
-    
+    elif call.data == "mypr":
+        try:
+            date = datetime.datetime.now().strftime("_%d_%m_%y")
 
+            with sqlite3.connect('database.db') as conn:
+                cu = conn.cursor() 
+                cu.execute(f"CREATE TABLE IF NOT EXISTS meals{date} (chat_id INTEGER, meal TEXT, info TEXT)")
+                cu.execute(f"SELECT meal,info FROM MEALS{date} WHERE chat_id = ?", (call.message.chat.id,))
+                meals = cu.fetchall()
+                if len(meals)>0:
+                    text = "Ваши приемы:\n"
+                    for meal in meals: 
+                        text += f"{meal[0]}: {meal[1]}\n"
+                    bot.send_message(call.message.chat.id, text,)
+                else: 
+                    bot.send_message(chat_id=call.message.chat.id,text='Приемов не найдено')
+                    bot.send_message(chat_id=call.message.chat.id, text = "Добрый день, выберите действие:", reply_markup=create_keyboard_main())
+        except:
+            bot.send_message(call.message.chat.id,'Неизвестная ошибка')
+
+    elif call.data == 'del': 
+        try:
+            date = datetime.datetime.now().strftime("_%d_%m_%y")
+            ch_id = call.message.chat.id 
+            with sqlite3.connect('database.db') as conn: 
+                cu = conn.cursor()
+                items = [*cu.execute(f'SELECT meal,rowid FROM meals{date} WHERE chat_id = ?',(ch_id,))]
+                cu.close()
+            bot.send_message(chat_id=ch_id, text='Выберите прием для удаления', reply_markup=create_keyboard_del(items))
+        except: 
+            bot.send_message(call.message.chat.id,'неизвестная ошибка')
+
+    elif call.data[0:4]=='del%':
+        try: 
+            rod = call.data.split('%')[1]
+            with sqlite3.connect('database.db') as conn: 
+                date = datetime.datetime.now().strftime("_%d_%m_%y")
+                cu = conn.cursor()
+                cu.execute(f'DELETE FROM meals{date} WHERE rowid=?',(rod,))
+                conn.commit()
+                cu.close()
+            bot.send_message(chat_id=call.message.chat.id, text = 'Прием был успешно удален',reply_markup=create_keyboard_back())
+        except: 
+            bot.send_message(call.message.chat.id,'неизвестная ошибка',reply_markup=create_keyboard_back())
 
 def add_meal(message):
     try: 
          meal, info = message.text.split(":")
     except :
-        bot.send_message(message.chat.id, "Неверный формат. Пожалуйста, используйте формат: Обед: 500/50/20/45")
+        bot.send_message(message.chat.id, "Неверный формат. Пожалуйста, используйте формат: Обед: 500 50 20 45")
         bot.register_next_step_handler(message, add_meal)
     date = datetime.datetime.now().strftime("_%d_%m_%y")
     conn = sqlite3.connect('database.db')
